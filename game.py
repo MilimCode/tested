3af6177b259623253acd3d4421e9f58c302f96d4
MilimCode <139664950+MilimCode@users.noreply.github.com>
2024-07-02 16:52:31 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,17 @@
+import os
+
+def number_guess_game():
+    while True:
+        print("===============================")
+        print("Welcome to the Number Guess Game!")
+        print("===============================")
+        guess = input("Guess a number between 1 and 10: ")
+        target = int.from_bytes(os.urandom(1), "big") % 10 + 1
+        if guess == str(target):
+            print("Correct! You guessed the number!")
+        else:
+            print(f"Sorry, the correct number was {target}.")
+        input("Press Enter to play again...")
+
+if __name__ == '__main__':
+    number_guess_game()
